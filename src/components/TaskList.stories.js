ed326222651fa0045adc352156861d1320aab392
whataboutweeks <ajw0512@gmail.com>
2021-07-20 16:56:38 -0400

    Screen component complete!

--- src/components/TaskList.stories.js
+++ src/components/TaskList.stories.js
@@ -4,7 +4,6 @@ import { TaskList } from './TaskList';
 import * as TaskStories from './Task.stories';
 
 export default {
-  component: TaskList,
   title: 'TaskList',
   decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
