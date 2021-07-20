94f57908210617f82e7e433515b2c980f765c711
whataboutweeks <ajw0512@gmail.com>
2021-07-20 16:54:19 -0400

    Actual data store hook up complete!

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -3,7 +3,10 @@ import PropTypes from 'prop-types';
 
 import Task from './Task';
 
-export default function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
+import { connect } from 'react-redux';
+import { archiveTask, pinTask } from '../lib/redux';
+
+export function TaskList({ loading, tasks, onPinTask, onArchiveTask }) {
   const events = {
     onPinTask,
     onArchiveTask,
@@ -62,3 +65,13 @@ TaskList.propType = {
 TaskList.defaultProps = {
   loading: false,
 };
+
+export default connect(
+  ({ tasks }) => ({
+    tasks: tasks.filter(t => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'),
+  }),
+  dispatch => ({
+    onArchiveTask: id => dispatch(archiveTask(id)),
+    onPinTask: id => dispatch(pinTask(id)),
+  })
+)(TaskList);
